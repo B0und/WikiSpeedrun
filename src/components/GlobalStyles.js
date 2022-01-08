efeb83247ff40aecffbf64143bfe0c0be6a6636d
Unbound <f1arenova1@gmail.com>
2022-01-08 19:27:10 +0300

    update global styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -1,81 +1,59 @@
 import { createGlobalStyle } from "styled-components/macro";
 
 const GlobalStyles = createGlobalStyle`
-/* http://meyerweb.com/eric/tools/css/reset/
-   v2.0 | 20110126
-   License: none (public domain)
-*/
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-  font-size: 100%;
-	vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, menu, nav, section {
-	display: block;
-}
-ol, ul {
-	list-style: none;
-}
-blockquote, q {
-	quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
 
+  * {
+    margin: 0;
+  }
 
-/* GLOBAL STYLES */
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  line-height: 1.5;
-  font-family: 'Raleway', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: auto;
-}
+  html, body {
+    height: 100%;
+  }
 
-#root {
-  /*
-    Create a stacking context, without a z-index.
-    This ensures that all portal content (modals and tooltips) will
-    float above the app.
+  body {
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+
+  /* Remove built-in form typography styles */
+  input, button, textarea, select {
+    font: inherit;
+  }
+
+  /* Avoid text overflows */
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+
+  /* Create a stacking context, without a z-index.
+    This ensures that all portal content (modals and tooltips) will float above the app.
   */
-  isolation: isolate;
-}
+  #root, #__next {
+    isolation: isolate;
+  }
 
-html {
-  /*
-    Silence the warning about missing Reach Dialog styles
-  */
-  --reach-dialog: 1;
-}
+  /* GLOBAL STYLES */
+  *,
+  *:before,
+  *:after {
+    font-family: 'Raleway', sans-serif;
+  }
 
-html, body, #root {
-  height: 100%;
-}
+  html {
+    /*
+      Silence the warning about missing Reach Dialog styles
+    */
+    --reach-dialog: 1;
+  }
+
 `;
 
 export default GlobalStyles;
