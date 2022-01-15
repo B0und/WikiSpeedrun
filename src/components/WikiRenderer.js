20537ee61873423a817b2689baa2cf23d63d6c30
Unbound <f1arenova1@gmail.com>
2022-01-15 23:53:15 +0300

    render article title

--- src/components/WikiRenderer.js
+++ src/components/WikiRenderer.js
@@ -2,11 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import {
-  selectEndingArticle,
-  selectStartingArticle,
-  start,
-} from "./settingsSlice";
+import styled from "styled-components/macro";
+import { selectEndingArticle, selectStartingArticle } from "./settingsSlice";
 import axios from "axios";
 
 import "./wiki-common.css";
@@ -16,33 +13,50 @@ function WikiRenderer() {
   const endingId = useSelector(selectEndingArticle).pageid;
   const startingId = useSelector(selectStartingArticle).pageid;
 
-  const [html, setHtml] = useState("");
+  const [wikiData, setWikiData] = useState("");
+
+  const search = async (startingId = undefined, searchString = undefined) => {
+    const resp = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+      params: {
+        pageid: startingId,
+        page: searchString,
+        origin: "*",
+        action: "parse",
+        format: "json",
+        disableeditsection: "true",
+      },
+    });
+    const html = resp.data.parse.text["*"];
+    const title = resp.data.parse.title;
+    const pageid = resp.data.parse.pageid;
+    setWikiData({ html, title, pageid });
+  };
 
   useEffect(() => {
-    const search = async () => {
-      const resp = await axios.get(`https://en.wikipedia.org/w/api.php`, {
-        params: {
-          pageid: startingId,
-          origin: "*",
-          action: "parse",
-          format: "json",
-          disableeditsection: "true",
-        },
-      });
-      console.log(resp);
-      const htmlString = resp.data.parse.text["*"];
-      setHtml(htmlString);
-    };
-    search();
+    search(startingId);
   }, []);
 
   function createMarkup() {
-    return { __html: html };
+    return { __html: wikiData.html };
   }
 
   return (
-    <div className="wiki-insert" dangerouslySetInnerHTML={createMarkup()} />
+    <WikiWrapper>
+      <WikiHeader>{wikiData.title}</WikiHeader>
+      <div className="wiki-insert" dangerouslySetInnerHTML={createMarkup()} />
+    </WikiWrapper>
   );
 }
 
+const WikiWrapper = styled.div`
+  margin-left: var(--border-gap);
+`;
+
+const WikiHeader = styled.h2`
+  font-size: 1.8rem;
+  font-weight: 400;
+  font-family: "Linux Libertine", "Georgia", "Times", "serif";
+  margin-bottom: 0.25em;
+  border-bottom: 1px solid #a2a9b1;
+`;
 export default WikiRenderer;
