d8443e5c8f4437200d7e4c99193d0901df8e742c
Unbound <f1arenova1@gmail.com>
2022-01-18 14:10:34 +0300

    add 404 component

---
+++ src/components/NoMatch.js
@@ -0,0 +1,11 @@
+const NoMatch = () => {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+};
+export default NoMatch;
