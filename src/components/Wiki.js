d6883cd0cd2f3e5bc4bf27da813b2e59039a878a
Unbound <f1arenova1@gmail.com>
2022-01-19 21:29:26 +0300

    fix full page reload due to wiki link tags

--- src/components/Wiki.js
+++ src/components/Wiki.js
@@ -9,13 +9,16 @@ import { useParams } from "react-router-dom";
 
 import "./wiki-common.css";
 import "./wiki-vec2.css";
+import { useNavigate } from "react-router-dom";
 import Result from "./Result";
 
 function WikiRenderer() {
   let params = useParams();
+  let navigate = useNavigate();
+
+  const [wikiData, setWikiData] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [showResults, setshowResults] = useState(false);
-  const [wikiData, setWikiData] = useState("");
 
   const startTitle = useSelector(selectStartingArticle).title;
   const endTitle = useSelector(selectEndingArticle).title;
@@ -29,6 +32,7 @@ function WikiRenderer() {
         action: "parse",
         format: "json",
         disableeditsection: "true",
+        redirects: "true", // automatically redirects from plural form
       },
     });
     const html = resp.data.parse.text["*"];
@@ -38,19 +42,37 @@ function WikiRenderer() {
     setIsLoading(false);
   };
 
+  // render initial wiki article
   useEffect(() => {
     search(startTitle);
   }, [startTitle]);
 
+  // render article from route parameters
   useEffect(() => {
     if (params.wikiTitle) {
       search(params.wikiTitle);
     }
   }, [params.wikiTitle]);
 
+  // track winning condition
+  useEffect(() => {
+    if (endTitle === wikiData.title) {
+      setshowResults(true);
+    }
+  }, [endTitle, wikiData]);
+
   function createMarkup() {
     return { __html: wikiData.html };
   }
+
+  const validateHref = (hrefText) => {
+    if (hrefText === undefined) return null;
+    if (hrefText.startsWith("/wiki/") && !hrefText.includes(".")) {
+      return hrefText;
+    } else {
+      return null;
+    }
+  };
 
   return (
     <WikiWrapper>
@@ -62,6 +84,15 @@ function WikiRenderer() {
         <>
           <WikiHeader>{wikiData.title}</WikiHeader>
           <div
+            onClick={(e) => {
+              e.preventDefault();
+              const href = validateHref(
+                e?.nativeEvent?.explicitOriginalTarget?.attributes[0]?.value
+              );
+              if (href) {
+                navigate(href);
+              }
+            }}
             className="wiki-insert"
             dangerouslySetInnerHTML={createMarkup()}
           />
