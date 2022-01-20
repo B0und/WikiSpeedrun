c15d53517e7bdb7691489cbd277d40e19ead5bfe
Unbound <f1arenova1@gmail.com>
2022-01-20 19:22:46 +0300

    change logo link path

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Logo() {
   return (
     <Heading>
-      <StyledLink to="/">
+      <StyledLink to="/settings">
         <Image
           src={window.location.origin + "/wiki-speed-logo.jpg"}
           alt="Wikipedia Speedrun"
