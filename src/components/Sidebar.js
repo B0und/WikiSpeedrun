9e88db19429a502a6eea5e0db63f148e54a3a4bf
Unbound <f1arenova1@gmail.com>
2022-01-24 23:29:03 +0300

    display # of clicks in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,26 @@
 import { useContext, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components/macro";
 import History from "./History.js";
 import Logo from "./Logo";
+import { selectHistory } from "./settingsSlice.js";
 import Stopwatch from "./Stopwatch";
 import { StopwatchContext } from "./StopwatchContext";
 
 function Sidebar() {
   const stopwatch = useContext(StopwatchContext);
+  const history = useSelector(selectHistory);
 
   return (
     <SidebarWrapper>
       <Logo />
       <History />
-      <Stopwatch time={stopwatch.time} />
+      <BottomWrapper>
+        <Clicks>
+          Clicks: {history.length === 0 ? "0" : history.length - 1}
+        </Clicks>
+        <Stopwatch time={stopwatch.time} />
+      </BottomWrapper>
     </SidebarWrapper>
   );
 }
@@ -31,4 +39,14 @@ const SidebarWrapper = styled.nav`
   padding-bottom: var(--border-gap);
 `;
 
+const BottomWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: baseline;
+`;
+
+const Clicks = styled.p`
+  font-size: ${18 / 16}rem;
+`;
 export default Sidebar;
