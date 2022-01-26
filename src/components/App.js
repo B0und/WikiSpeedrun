bfcf335f41fb82c7eadfdf0eb19a4c6234772603
Unbound <f1arenova1@gmail.com>
2022-01-27 01:48:45 +0300

    add funny

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import NoMatch from "./NoMatch";
@@ -8,6 +8,12 @@ import About from "./About";
 const Wiki = React.lazy(() => import("./Wiki"));
 
 function App() {
+  useEffect(() => {
+    console.log(
+      "%cWikiSpeedrunGame",
+      "font-weight: bold; background-color:#0e0d0d; font-size: 42px;color: #4acd79; text-shadow: 3px 3px 0 #33a75e , 6px 6px 0 #13793a , 9px 9px 0 #094d22; padding: 5%"
+    );
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
