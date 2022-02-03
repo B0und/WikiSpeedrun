e61977b787b60f105fa683f5c9a43181277324d3
Unbound <f1arenova1@gmail.com>
2022-02-04 02:42:18 +0300

    upd nomatch component

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components/macro";
 
 const NoMatch = () => {
   return (
-    <div>
+    <Wrapper>
       <h2>Nothing to see here!</h2>
       <p>
         <Link to="/">Go to the home page</Link>
       </p>
-    </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  padding-left: var(--border-gap);
+  padding-top: 8px;
+`;
 export default NoMatch;
