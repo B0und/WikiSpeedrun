f0e4f679032956c7db3197b40337eec6837a5c9a
Unbound <f1arenova1@gmail.com>
2022-02-06 19:02:24 +0300

    update about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,27 @@ function About() {
   return (
     <Wrapper>
       <AboutHeader>Wiki Speedrun Game</AboutHeader>
-      <div>
+      <p>
         The goal is simple: using links, navigate from one wiki article to
         another one, as fast as you can!
-      </div>
+      </p>
+
+      <SecondaryHeading>Features</SecondaryHeading>
+
+      <FeatureList>
+        <li>No registration required</li>
+        <li>Choose your own prompts</li>
+        <li>
+          High precision fair™ timer
+          <ul>
+            <li>actually stops while you are loading the next article</li>
+          </ul>
+        </li>
+        <li>Optional Time Limit</li>
+        <li>Keeps track of your session progress</li>
+        <li>Open source</li>
+      </FeatureList>
+
       <StyledLinkButton text="Play" to={"/settings"} />
     </Wrapper>
   );
@@ -25,12 +42,27 @@ const Wrapper = styled.div`
 `;
 
 const AboutHeader = styled(WikiHeader)`
-  border-bottom: 1px solid #a2a9b1;
+  border-bottom: 1px solid #a2a9b178;
   margin-bottom: 0.25em;
 `;
 
 const StyledLinkButton = styled(LinkButton)`
   background: var(--secondary-blue);
   color: black;
+`;
+
+const SecondaryHeading = styled.h3`
+  font-size: ${24/16}rem;
+  font-weight: 400;
+
+  border-bottom: 1px solid #a2a9b178;
+  margin-top: 16px;
+  margin-bottom: 0.25em;
+`;
+
+const FeatureList = styled.ul`
+  li:not(:last-child) {
+    margin-bottom: 8px;
+  }
 `;
 export default About;
