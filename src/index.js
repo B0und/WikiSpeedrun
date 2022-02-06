f1a88dea8483857ab7e910a975f964431ecf1c52
Unbound <f1arenova1@gmail.com>
2022-02-06 18:32:39 +0300

    remove unused packages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import GlobalStyles from "./components/GlobalStyles";
-import reportWebVitals from "./reportWebVitals";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { StopwatchProvider } from "./components/Stopwatch/StopwatchContext";
@@ -26,7 +25,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
