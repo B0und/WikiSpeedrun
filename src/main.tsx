629619d3dd2845e32e9ff5ddfb68da58ee4a2ed9
Vlad Moiseenko <f1arenova1@gmail.com>
2023-01-03 23:18:42 +0300

    add i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
+import TypesafeI18n from "./i18n/i18n-react"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <TypesafeI18n locale="en">
+      <App />
+    </TypesafeI18n>
+  </React.StrictMode>
 )
