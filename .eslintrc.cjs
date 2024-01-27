96b70ccebe607c09b9cb49868f2455f283d52c82
Unbound <unbound9dev@gmail.com>
2024-01-27 22:55:41 +0300

    wip

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,9 +12,8 @@ module.exports = {
 
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:@tanstack/eslint-plugin-query/recommended"
   ],
   overrides: [],
@@ -22,12 +21,13 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    "project": true
   },
   plugins: ["react", "@typescript-eslint", "@tanstack/query"],
   rules: {
     // suppress errors for missing 'import React' in files
-    "react/react-in-jsx-scope": "off",
+    // "react/react-in-jsx-scope": "off",
     // allow jsx syntax in js files (for next.js project)
-    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }], //should add ".ts" if typescript project
+    // "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }], //should add ".ts" if typescript project
   },
 };
