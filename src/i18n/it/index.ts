32edf13980bd5e60b78ba49716af9e2b2977c007
Jackmo04 <86155838+Jackmo04@users.noreply.github.com>
2024-04-02 09:39:20 +0200

    Improved Italian translation
    The previous translation wasn't very good, I'm assuming it was made using some kind of online translator. This one was made by me as an Italian.

--- src/i18n/it/index.ts
+++ src/i18n/it/index.ts
@@ -2,13 +2,13 @@ import { Translation } from "../i18n-types";
 
 // Italian
 const it: Translation = {
-  "Wiki Speedrun Game": "Gioco di Speedrun di Wikipedia",
+  "Wiki Speedrun Game": "Speedrun di Wikipedia",
   "The goal of the game is to navigate from a starting wikipedia article to another one, in the least amount of clicks and time":
-    "L'obiettivo del gioco è navigare da un articolo iniziale di Wikipedia a un altro, con il minor numero di clic e tempo possibile.",
+    "L'obiettivo del gioco è navigare da un articolo di Wikipedia iniziale ad uno finale, nel minor numero di clic e tempo possibili.",
   Features: "Caratteristiche",
-  "Now supports multiple languages": "Supporta ora più lingue",
-  "No registration required": "Nessuna registrazione richiesta",
-  "High precision fair™ timer": "Cronometro da fiera ad alta precisione",
+  "Now supports multiple languages": "Ora supporta più lingue",
+  "No registration required": "Nessuna registrazione necessaria",
+  "High precision fair™ timer": "Cronometro onesto™ ad alta precisione",
   "actually stops while you are loading the next article":
     "si ferma effettivamente mentre stai caricando il prossimo articolo",
   "Keeps track of your session progress": "Tiene traccia del progresso della sessione",
@@ -20,36 +20,36 @@ const it: Translation = {
   "Start typing and then select values from the dropdown list or press the random button":
     "Inizia a digitare e poi seleziona i valori dall'elenco a discesa o premi il pulsante casuale.",
   "Select starting article": "Seleziona l'articolo di partenza",
-  "Select ending article": "Seleziona l'articolo finale",
-  "Start typing to see options": "Inizia a digitare per vedere le opzioni",
+  "Select ending article": "Seleziona l'articolo di arrivo",
+  "Start typing to see options": "Inizia a digitare per vedere delle opzioni",
   "Article clicks": "Click sugli articoli",
-  "Cheating attempts": "Tentativi di frode",
+  "Cheating attempts": "Tentativi di imbroglio",
   Results: "Risultati",
   "Play again": "Gioca di nuovo",
-  "Give up": "Arrendersi",
+  "Give up": "Arrenditi",
   "Confirm action": "Conferma azione",
-  "If you leave, your current progress will be lost": "Se lasci, perderai il progresso attuale",
+  "If you leave, your current progress will be lost": "Se abbandoni, i tuoi progressi attuali saranno persi",
   History: "Cronologia",
   Article: "Articolo",
   Time: "Tempo",
   "This page doesn't exist": "Questa pagina non esiste",
   "Clicks: {0}": "Click: {0}",
   "No articles found": "Nessun articolo trovato",
-  "Get random article": "Ottieni un articolo casuale",
+  "Get random article": "Scegli un articolo casuale",
   "Wiki speedrun logo, featuring a Wikipedia sphere with a timer across i (looks like a big black stripe with a green time text on top) The time is 9 seconds and 5 milliseconds":
-    "Logo del wiki speedrun, che mostra una sfera di Wikipedia con un timer attraverso di essa (sembra una grande striscia nera con un testo verde del tempo in alto). Il tempo è di 9 secondi e 5 millisecondi.",
+    "Logo di Speedrun Wikipedia, che mostra una sfera di Wikipedia con un timer al centro di essa (sembra una grande striscia nera con un testo verde per il tempo in alto). Il tempo è di 9 secondi e 5 millisecondi.",
   "Select article language": "Seleziona la lingua dell'articolo",
   Language: "Lingua",
   Navigation: "Navigazione",
   "No Cheating!": "Niente imbrogli!",
   "Random failed, try again": "Selezione casuale fallita, riprova",
-  "Choose another link": "Scegli un altro collegamento",
+  "Choose another link": "Scegli un altro link",
   "Share settings": "Condividi impostazioni",
   "Copied to clipboard": "Copiato negli appunti",
   "Share Result": "Condividi risultato",
   Loading: "Caricamento...",
   "Couldn't load article preview": "Impossibile caricare l'anteprima dell'articolo",
-  "Winning links": "Collegamenti vincenti",
+  "Winning links": "Link vincenti",
   "Missed wins": "Vittorie mancate",
   "Choose your article": "Scegli il tuo articolo",
   Statistics: "Statistiche",
@@ -61,60 +61,60 @@ const it: Translation = {
   "Previewed Articles:": "Articoli visualizzati in anteprima:",
   Achievements: "Obiettivi",
   "All of the information is stored locally in your browser (because servers cost money)":
-    "Tutte le informazioni sono memorizzate localmente nel tuo browser (perché i server costano denaro)",
+    "Tutte le informazioni sono memorizzate localmente nel tuo browser (perché i server costano soldi)",
   "If you clear your browser data or switch to another browser all of your data will be gone":
     "Se cancelli i dati del tuo browser o passi a un altro browser, tutti i tuoi dati saranno persi.",
   FirstWin: {
     title: "Prima Vittoria",
-    description: "Completa il tuo primo speedrun",
+    description: "Completa la tua prima speedrun",
   },
   NoviceRunner: {
-    title: "Corridore Novizio",
-    description: "Vinci 10 giochi",
+    title: "Speedrunner Novizio",
+    description: "Vinci 10 partite",
   },
   Speedster: {
     title: "Velocista",
-    description: "Vinci 25 giochi",
+    description: "Vinci 25 partite",
   },
   WikiExplorer: {
     title: "Esploratore di Wiki",
-    description: "Vinci 50 giochi",
+    description: "Vinci 50 partite",
   },
   SpeedDemon: {
     title: "Demone della Velocità",
-    description: "Vinci 100 giochi",
+    description: "Vinci 100 partite",
   },
   MasterRunner: {
     title: "Corridore Maestro",
-    description: "Vinci 250 giochi",
+    description: "Vinci 250 partite",
   },
   WikipediaChampion: {
     title: "Campione di Wikipedia",
-    description: "Vinci 500 giochi",
+    description: "Vinci 500 partite",
   },
   SpeedrunAddict: {
-    title: "Dipendente dal Speedrun",
-    description: "Vinci 1000 giochi",
+    title: "Dipendente dalle Speedrun",
+    description: "Vinci 1000 partite",
   },
   WikipediaLegend: {
     title: "Leggenda di Wikipedia",
-    description: "Vinci 2500 giochi",
+    description: "Vinci 2500 partite",
   },
   SpeedrunGod: {
-    title: "Dio del Speedrun",
-    description: "Vinci 5000 giochi",
+    title: "Dio delle Speedrun",
+    description: "Vinci 5000 partite",
   },
   AttentiveExplorer: {
     title: "Esploratore Attento",
-    description: "Naviga attraverso almeno 10 articoli senza perdere il collegamento vincente",
+    description: "Naviga attraverso almeno 10 articoli senza mancare il link vincente",
   },
   KeenPathfinder: {
-    title: "Percorritore Attento",
-    description: "Naviga attraverso almeno 25 articoli senza perdere il collegamento vincente",
+    title: "Pioniere Attento",
+    description: "Naviga attraverso almeno 25 articoli senza mancare il link vincente",
   },
   SharpNavigator: {
     title: "Navigatore Acuto",
-    description: "Naviga attraverso almeno 50 articoli senza perdere il collegamento vincente",
+    description: "Naviga attraverso almeno 50 articoli senza mancare il link vincente",
   },
   ExplorerOfChance: {
     title: "Esploratore del Caso",
@@ -134,19 +134,19 @@ const it: Translation = {
   },
   Curiosity: {
     title: "Curiosità",
-    description: "Anteprima di un articolo",
+    description: "Guarda l'anteprima di un articolo",
   },
   CuriousExplorer: {
     title: "Esploratore Curioso",
-    description: "Anteprima di 100 articoli",
+    description: "Guarda l'anteprima di 100 articoli",
   },
   PreviewEnthusiast: {
     title: "Appassionato di Anteprime",
-    description: "Anteprima di 1000 articoli",
+    description: "Guarda l'anteprima di 1000 articoli",
   },
   InsatiablesReader: {
     title: "Lettore Insaziabile",
-    description: "Anteprima di 10000 articoli",
+    description: "Guarda l'anteprima di 10000 articoli",
   },
   Bilingual: {
     title: "Bilingue",
@@ -161,7 +161,7 @@ const it: Translation = {
     description: "Esplora articoli in 5 lingue diverse",
   },
   EgoStroke: {
-    title: "Lusinga dell'Ego",
+    title: "Colpo di Ego",
     description: "Grazie per aver ispirato tutto questo progetto",
   },
   SpeedrunWaifu: {
@@ -173,7 +173,7 @@ const it: Translation = {
   "twitter (X)": " Twitter (X)",
   "Achievement unlocked": "Obiettivo sbloccato",
   WaifuAlt:
-    "Carina ragazza anime con un vestito blu seduta in una biblioteca, mentre un globo Wikipedia con un timer di speedrun allegato fluttua nelle vicinanze",
+    "Ragazza anime carina con un vestito blu seduta in una biblioteca, mentre un globo di Wikipedia con un timer per speedrun al suo interno fluttua nelle vicinanze",
   "Prize trophy": "Trofeo premio",
 };
 
