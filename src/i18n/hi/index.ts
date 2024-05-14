739005f8bb1f81111287db998b2f406b786c6d13
prasantahembram <Prasantahembram720@gmail.com>
2024-05-14 17:48:58 +0530

    Added Hindi locale
    Added Hindi locale

---
+++ src/i18n/hi/index.ts
@@ -0,0 +1,181 @@
+import type { BaseTranslation } from "../i18n-types";
+
+// Hindi
+const hi: BaseTranslation = {
+  "Wiki Speedrun Game": "विकी स्पीडरन गेम",
+  "The goal of the game is to navigate from a starting wikipedia article to another one, in the least amount of clicks and time":
+    "गेम का लक्ष्य कम से कम क्लिक और समय में एक प्रारंभिक विकिपीडिया लेख से दूसरे लेख तक नेविगेट करना है",
+  Features: "विशेषताएँ",
+  "Now supports multiple languages": "अब अनेक भाषाओं का समर्थन करता है",
+  "No registration required": "पंजीकरण की आवश्यकता नहीं है",
+  "High precision fair™ timer": "उच्च परिशुद्धता निष्पक्ष™ टाइमर",
+  "actually stops while you are loading the next article":
+    "वास्तव में जब आप अगला लेख लोड कर रहे होते हैं तो रुक जाता है",
+  "Keeps track of your session progress": "आपके सत्र की प्रगति पर नज़र रखता है",
+  "Dark theme support": "डार्क थीम समर्थन",
+  "Open source": "खुला स्त्रोत",
+  Play: "चालू करें",
+  About: "के बारे में",
+  Settings: "समायोजन",
+  "Start typing and then select values from the dropdown list or press the random button":
+    "टाइप करना प्रारंभ करें और फिर ड्रॉपडाउन सूची से मान चुनें या यादृच्छिक बटन दबाएँ",
+  "Select starting article": "आरंभिक लेख चुनें",
+  "Select ending article": "अंतिम लेख का चयन करें",
+  "Start typing to see options": "विकल्प देखने के लिए टाइप करना प्रारंभ करें",
+  "Article clicks": "लेख पर दबाए गए",
+  "Cheating attempts": "धोखाधड़ी के प्रयास",
+  Results: "परिणाम",
+  "Play again": "फिर से चालू करें",
+  "Give up": "छोड़ देना",
+  "Confirm action": "कार्रवाई की पुष्टि करें",
+  "If you leave, your current progress will be lost":
+    "यदि आप चले गए तो आपकी वर्तमान प्रगति नष्ट हो जाएगी",
+  History: "इतिहास",
+  Article: "लेख",
+  Time: "समय",
+  "This page doesn't exist": "यह पृष्ठ अस्तित्व में नहीं है",
+  "Clicks: {0}": "दबाए गए: {0}",
+  "No articles found": "कोई लेख नहीं मिला",
+  "Get random article": "यादृच्छिक लेख प्राप्त करें",
+  "Wiki speedrun logo, featuring a Wikipedia sphere with a timer across i (looks like a big black stripe with a green time text on top) The time is 9 seconds and 5 milliseconds":
+    "विकी स्पीडरन लोगो, जिसमें एक विकिपीडिया क्षेत्र है जिसके चारों ओर एक टाइमर लगा हुआ है। (शीर्ष पर हरे समय के पाठ के साथ एक बड़ी काली पट्टी जैसा दिखता है)। समय 9 सेकंड और 5 मिलीसेकंड है।",
+  "Select article language": "लेख की भाषा चुनें",
+  Language: "भाषा",
+  Navigation: "मार्गदर्शन",
+  "No Cheating!": "कोई धोखा नहीं!",
+  "Random failed, try again": "आकस्मिक रूप से विफल, पुनः प्रयास करें",
+  "Choose another link": "दूसरा लिंक चुनें",
+  "Share settings": "सेटिंग साझा करें",
+  "Copied to clipboard": "क्लिपबोर्ड पर नकल",
+  "Share Result": "परिणाम साझा करें",
+  Loading: "लोडिंग...",
+  "Couldn't load article preview": "आलेख पूर्वावलोकन लोड नहीं किया जा सका",
+  "Winning links": "जीतने वाले लिंक",
+  "Missed wins": "जीत चूक गई",
+  "Choose your article": "अपना लेख चुनें",
+  Statistics: "आंकड़े",
+  "Wins:": "जीत:",
+  "Total games:": "कुल खेल:",
+  "Known languages:": "ज्ञात भाषाएँ:",
+  "Random choices:": "यादृच्छिक विकल्प:",
+  "Articles clicked:": "दबाए गए लेख:",
+  "Previewed Articles:": "समीक्षित लेख:",
+  Achievements: "उपलब्धियां",
+  "All of the information is stored locally in your browser (because servers cost money)":
+    "सभी जानकारी आपके ब्राउज़र में स्थानीय रूप से संग्रहीत की जाती है (क्योंकि सर्वर में पैसे खर्च होते हैं)",
+  "If you clear your browser data or switch to another browser all of your data will be gone":
+    "यदि आप अपना ब्राउज़र डेटा साफ़ करते हैं या किसी अन्य ब्राउज़र पर स्विच करते हैं, तो आपका सारा डेटा चला जाएगा।",
+  // ACHIEVEMENTS
+  FirstWin: {
+    title: "पहली जीत",
+    description: "Complete your first speedrun",
+  },
+  NoviceRunner: {
+    title: "नौसिखिया धावक",
+    description: "10 खेल जीतें",
+  },
+  Speedster: {
+    title: "स्पीडस्टर",
+    description: "25 खेल जीतें",
+  },
+  WikiExplorer: {
+    title: "विकी एक्सप्लोरर",
+    description: "50 खेल जीतें",
+  },
+  SpeedDemon: {
+    title: "गति दानव",
+    description: "100 खेल जीतें",
+  },
+  MasterRunner: {
+    title: "मास्टर धावक",
+    description: "250 खेल जीतें",
+  },
+  WikipediaChampion: {
+    title: "विकिपीडिया चैंपियन",
+    description: "500 खेल जीतें",
+  },
+  SpeedrunAddict: {
+    title: "स्पीडरन एडिक्ट",
+    description: "1000 खेल जीतें",
+  },
+  WikipediaLegend: {
+    title: "विकिपीडिया लीजेंड",
+    description: "2500 खेल जीतें",
+  },
+  SpeedrunGod: {
+    title: "स्पीडरन भगवान",
+    description: "5000 खेल जीतें",
+  },
+  AttentiveExplorer: {
+    title: "चौकस खोजकर्ता",
+    description: "विजेता लिंक को खोए बिना कम से कम 10 लेखों पर नेविगेट करें",
+  },
+  KeenPathfinder: {
+    title: "उत्सुक पथप्रदर्शक",
+    description: "विजेता लिंक को खोए बिना कम से कम 25 लेखों पर नेविगेट करें",
+  },
+  SharpNavigator: {
+    title: "तीव्र नेविगेटर",
+    description: "विजेता लिंक को खोए बिना कम से कम 50 लेखों पर नेविगेट करें",
+  },
+  ExplorerOfChance: {
+    title: "संभावना का अन्वेषक",
+    description: "10 यादृच्छिक लेख चुनें",
+  },
+  FortuneSeeker: {
+    title: "भाग्य साधक",
+    description: "100 यादृच्छिक लेख चुनें",
+  },
+  GachaAddict: {
+    title: "गच्चा दीवाना",
+    description: "1000 यादृच्छिक लेख चुनें",
+  },
+  GachaOverlord: {
+    title: "गचा अधिपति",
+    description: "10000 यादृच्छिक लेख चुनें",
+  },
+  Curiosity: {
+    title: "जिज्ञासु होना कभी-कभी खतरे या दुर्भाग्य का कारण बन सकता है",
+    description: "किसी लेख का पूर्वावलोकन करें",
+  },
+  CuriousExplorer: {
+    title: "जिज्ञासु अन्वेषक",
+    description: "100 लेखों का पूर्वावलोकन करें",
+  },
+  PreviewEnthusiast: {
+    title: "पूर्वावलोकन उत्साही",
+    description: "1000 लेखों का पूर्वावलोकन करें",
+  },
+  InsatiablesReader: {
+    title: "अतृप्त पाठक",
+    description: "10000 लेखों का पूर्वावलोकन करें",
+  },
+  Bilingual: {
+    title: "द्विभाषिक",
+    description: "2 अलग-अलग भाषाओं में लेख खोजें",
+  },
+  Trilingual: {
+    title: "त्रिभाषी",
+    description: "3 अलग-अलग भाषाओं में लेखों का अन्वेषण करें",
+  },
+  Polyglot: {
+    title: "बहुभाषी",
+    description: "5 विभिन्न भाषाओं में लेखों का अन्वेषण करें",
+  },
+  EgoStroke: {
+    title: "अहंकार आघात",
+    description: "इस पूरे प्रोजेक्ट को प्रेरित करने के लिए धन्यवाद",
+  },
+  SpeedrunWaifu: {
+    title: "वीक स्पीडरन वेफू",
+    description: "इना_डेन द्वारा निर्मित। उसे ट्विटर पर फ़ॉलो करें",
+  },
+  "Made by Ina_den": "इना_डेन द्वारा निर्मित।",
+  "Follow him on": "उसका अनुसरण करें",
+  "twitter (X)": " twitter (X)",
+  "Achievement unlocked": "खुली उपलब्धि",
+  WaifuAlt:
+    "नीली पोशाक वाली प्यारी एनीमे लड़की लाइब्रेरी में बैठी है, जबकि स्पीडरन टाइमर के साथ एक विकिपीडिया ग्लोब पास में तैर रहा है",
+  "Prize trophy": "पुरस्कार ट्रॉफी",
+};
+export default hi;
